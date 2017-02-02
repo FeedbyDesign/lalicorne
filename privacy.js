9b8df628679a4f6562633d730669c7f84ea29f71
Marc Coet <marcoet@gmail.com>
2017-02-02 12:17:48 +0100

    changed href problem on phone number

--- privacy.js
+++ privacy.js
@@ -1,17 +1,11 @@
 var phoneNode = document.getElementById('phone')
 var emailNode = document.getElementById('email')
-console.log(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent));
+
 if (window) {
   phoneNode.innerHTML = "+32478811015"
   emailNode.innerHTML = "info@lalicorne.net"
   emailNode.href = "mailto:info@lalicorne.net"
   if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-    console.log("tralala");
-    // phoneNode.href = "bbb"
+    phoneNode.href = "tel:+32478811015"
   }
 }
-
-
-// <a href="http://lalicorne.net">lalicorne.net</a> -
-// <a id="phone" href="+32478811015">+32478811015</a> -
-// <a href="info@lalicorne.net" target="_top">info@lalicorne.net</a>
